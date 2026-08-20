72e45e2e9dd6bfeb26c1ccc4ca6be0e1c9ada1ae
Yue Sheng <yueshengys@google.com>
2026-08-19 17:32:33 -0700

    [Mosaic TPU] Support packed types strided load with static indices by emulation.
    PiperOrigin-RevId: 967494773

--- tests/pallas/tpu_pallas_test.py
+++ tests/pallas/tpu_pallas_test.py
@@ -4311,6 +4311,62 @@ class MiscellaneousTest(ptu.PallasTPUTest):
     expected = np.concatenate(rolled_splits, axis=0)
     np.testing.assert_array_equal(out, expected)
 
+  @parameterized.product(
+      shape=((24, 128), (64, 128), (6, 64, 128)),
+      slice_size=(4, 8, 16),
+      # zero stride means broadcast.
+      strides=(
+          (0,),
+          (1,),
+          (2,),
+          (3,),
+          (4,),
+          (5,),
+          (6,),
+          (7,),
+          (8,),
+          (1, 0),
+          (2, 0),
+          (0, 2),
+          (1, 2),
+          (2, 1),
+          (2, 3),
+          (3, 2),
+          (0, 0),
+      ),
+      dtype=(jnp.float32, jnp.bfloat16, jnp.int8),
+  )
+  def test_strided_load(self, shape, slice_size, strides, dtype):
+    if self.INTERPRET and 0 in strides:
+      self.skipTest('Interpret mode does not support stride 0.')
+    if dtype != jnp.float32 and not jtu.is_libtpu_at_least('0.0.47'):
+      self.skipTest('Needs a newer libtpu')
+    if len(strides) != len(shape) - 1:
+      self.skipTest('Stride rank does not match shape rank')
+    slice_sizes = (slice_size,) if len(shape) == 2 else (2, slice_size)
+    for sz, st, dim in zip(slice_sizes, strides, shape[:-1]):
+      if sz * st > dim:
+        self.skipTest('Out-of-bounds stride for slice size')
+
+    x = jax.random.randint(jax.random.key(0), shape, -128, 127).astype(dtype)
+
+    def kernel(x_ref, o_ref):
+      idx = tuple(
+          pl.ds(0, sz, stride=st) for sz, st in zip(slice_sizes, strides)
+      ) + (slice(None),)
+      o_ref[...] = x_ref[idx]
+
+    out_shape = (*slice_sizes, shape[-1])
+    f = self.pallas_call(
+        kernel, out_shape=jax.ShapeDtypeStruct(out_shape, dtype)
+    )
+    out = jax.jit(f)(x)
+    np_indices = tuple(
+        np.arange(sz) * st for sz, st in zip(slice_sizes, strides)
+    ) + (np.arange(shape[-1]),)
+    expected = x[np.ix_(*np_indices)]
+    np.testing.assert_array_equal(out, expected)
+
   def test_retiling1(self):
     x = np.arange(1024, dtype=jnp.bfloat16).reshape(1024)
 
